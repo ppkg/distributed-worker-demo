1d70bcb58f6dbd099191130a2e8b092961e80e29
zihua <chendh@rp-pet.cn>
2022-01-17 19:35:12 +0800

    优化代码

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module distributed-worker-demo
 go 1.16
 
 require (
-	github.com/ppkg/distributed-worker v0.0.0-20220117070110-08990f9b99a9
+	github.com/ppkg/distributed-worker v0.0.0-20220117105913-7aeaa45bb4e3
 	github.com/ppkg/kit v0.0.0-20210823101229-cd1ff728fbc4
 )
--- sync_request.go
+++ sync_request.go
@@ -47,7 +47,7 @@ func regSyncRequest() {
 		fmt.Fprintf(rw, "同步请求完成，时间:%s，耗时：%f秒，job状态:%d，返回结果:%s <br/>", endTime.Format("2006-01-02 15:04:05"), endTime.Sub(startTime).Seconds(), resp.Status, resp.Result)
 
 		fmt.Fprintf(rw, "-------------------------------------------------------------<br>")
-		fmt.Fprintf(rw, "最终计算结果：%d <br/>", computeResult(resp.Result))
+		fmt.Fprintf(rw, "jobId:%d,最终计算结果：%d <br/>",resp.Id, computeResult(resp.Result))
 	})
 }
 
